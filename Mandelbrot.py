49811040b6d507f34e8f1f587c8e174b0dbd08cd
andersinde <33434572+andersinde@users.noreply.github.com>
2019-05-16 21:52:52 +0200

    Add files via upload

---
+++ Mandelbrot.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from numba import jit
+
+# this function is outside of the class so that we can use jit
+@jit
+def _determineDivergence(cr, ci, maxIter, horizon2, log_horizon):
+	"""returns number of iterations until loop break for a given
+	 complex number c = cr + i*ci"""
+
+	(zr, zi) = (cr, ci) # z after first iteration
+	for n in range(maxIter):
+		zr2 = zr*zr
+		zi2 = zi*zi
+		if zr2 + zi2 > horizon2:
+			return n + 1 - np.log(np.log(zr2 + zi2))/0.602 + log_horizon
+		zi = 2*zr*zi + ci
+		zr = zr2 - zi2 + cr
+	return 0
+
+class Mandelbrot:
+	"""A class representing an object used for customizing and plotting a mandelbrot set"""
+
+	def __init__(self, width=9, height=6, dpi=72, extent=[-2,1,-1,1], maxIter=128, cmap="cubehelix", zoom=2):
+		self.width = width
+		self.height = height
+		self.dpi = dpi
+
+		self.extent = extent
+		self.maxIter = maxIter
+		self.cmap = cmap
+		self.zoom = zoom
+		self._start = True
+
+	def getMandelbrotSet(self):
+		"""Performs _isInMandelbrotSet on every pixel and returns the result in an array"""
+		(x_min, x_max, y_min, y_max) = [i for i in self.extent]
+		(rows, columns) = (self.width*self.dpi, self.width*self.dpi)
+		r_real = np.linspace(x_min, x_max, columns)
+		r_imag = np.linspace(y_max, y_min, rows)
+
+		result = np.empty((rows, columns))
+
+		# used for smoother colors
+		horizon = 2.0**40
+		horizon2 = horizon**2
+		log_horizon = np.log(np.log(horizon))/np.log(2)
+
+		for i in range(rows): # imaginary values
+			for j in range(columns): # real values
+				result[i,j] = _determineDivergence(r_real[j], r_imag[i], self.maxIter, horizon2, log_horizon)
+		return result
+
+	def _handleClick(self, event):
+		""""zoom in and update image when clicking on figure"""
+
+
+		(x_min, x_max, y_min, y_max) = [i for i in self.extent]
+		if event.xdata != None and event.ydata != None:
+			(click_x, click_y) = (event.xdata, event.ydata)
+			newWidth = (x_max-x_min)/self.zoom
+			newHeight = (y_max-y_min)/self.zoom
+
+			# update self.extent to the new zoomed in extent
+			self.extent = [click_x-newWidth/2, click_x+newWidth/2, click_y-newHeight/2, click_y+newHeight/2]
+			self.plot()
+
+	def plot(self):
+		"""Plot using matplotlib"""
+
+		mandelbrotSet = self.getMandelbrotSet()
+
+		if self._start: # the first time this function is called, setup the figure
+			self._start = False
+			# width = 10
+			# height = width*self.rows/self.columns
+
+			# # fig = plt.figure(figsize=(self.width, self.height), dpi=self.dpi)
+			# fig = plt.figure(dpi=self.dpi)
+			# # fig.set_size_inches(self.width, self.height)
+
+			fig, ax = plt.subplots(figsize=(self.width, self.height))
+
+			fig.canvas.mpl_connect('button_press_event', self._handleClick) # connect zoom interaction
+
+			norm = colors.PowerNorm(0.3)
+			plt.imshow(mandelbrotSet, extent=self.extent, interpolation="nearest", cmap=self.cmap, norm=norm)
+			plt.title("Click where you want to zoom in")
+			plt.show()
+
+		else: # just redraw the image
+			norm = colors.PowerNorm(0.3)
+			plt.imshow(mandelbrotSet, extent=self.extent, interpolation="nearest", cmap=self.cmap, norm=norm)
+			plt.draw()
+
+if __name__ == "__main__":
+
+	extent = [-2,1,-1,1] # full size
+	# extent = [-0.35,0.08,0.63,1] # top hyperbolic
+	# extent = [-0.1443,-0.1432,-0.8389,-0.8379]
+
+	mandel = Mandelbrot(extent=extent, dpi=128, maxIter=400, cmap="gnuplot2", zoom=5)
+	mandel.plot()
---
+++ README.md
@@ -0,0 +1,57 @@
+### ovn7
+
+## A customizable Mandelbrot image generator in python
+
+Use this library to create a Mandelbrot set.
+
+### Installation
+
+```python
+import Mandelbrot
+```
+
+### Dependencies
+- python 3.7
+- numpy 1.16.2
+- matplotlib 3.0.3
+- numba 0.43.1
+
+NOTE: This project has only been tested using: python 3.7, numpy 1.16.2, matplotlib 3.0.3 and numba 0.43.1
+
+### Documentation
+
+#### Class constructor
+Create new instance of the Mandelbrot class
+```python
+def __init__(width=9, height=6, dpi=72, extent=[-2,1,-1,1], maxIter=128, cmap="cubehelix", zoom=2):
+```
+Parameters:
+ - **width** - width of figure in inches
+ - **height** - height of figure in inches
+ - **dpi** - dot per inch i.e. a measurement for resolution
+ - **extent** - an array of the boundary coordinates: [real_min, real_max, imaginary_min, imaginary_max]
+ - **maxIter** - maximum iteration used in calculation, a larger value will render more detail
+ - **cmap** - the colormap, used for customizing the color of the image, see [matplotlib.org/users/colormaps](https://matplotlib.org/users/colormaps.html) to find one that you prefer
+
+#### Plot the image
+Opens an interactive figure where you can zoom in
+```python
+def plot()
+```
+
+#### Basic example
+
+```python
+import Mandelbrot as mb # import module
+
+extent = [-0.40, 0.15, 0.63, 1]
+mandel = mb.Mandelbrot(extent=extent, cmap="magma") # create instance
+mandel.plot()
+```
+This will create a matplotlib figure like this:  
+
+![image1](images/image1.png)  
+
+Clicking on the small build on the top gives you this:  
+
+![image2](images/image2.png)  
